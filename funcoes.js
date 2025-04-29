366aa5de67cd3fc15b9f3df873aa644a46d07d34
unknown <arturlobo0522@digitalcollege.internal>
2025-04-29 14:35:51 -0300

    funções callback exemplos

--- funcoes.js
+++ funcoes.js
@@ -30,4 +30,41 @@ console.log(subtrair(15,5)) //onde  eu chamar a função vai ser substituido pel
 
 
 // const somar = function () => { //arrow funtion (função de sete)
+
+//outro exemplo de funções
+
+let escopoExterno = "Tenho uma variável de escopo global"
+
+function testeEscopo () {
+    let escopoInterno = "esta variável possui escopo local"
+    console.log (escopoExterno)
+    console.logo (escopoInterno)
     
+}
+
+testeEscopo()
+
+// console.log(escopoExterno)
+// console.log(escopoInterno)
+   
+//funções também são chamadas de métodos
+//no javascript tudo é objeto
+
+alert(`Olá`) //valor de parametro é default
+prompt(`digite seu nome`) //valor de parâmetro muda
+
+
+function somar(a,b) {
+    return a + b 
+
+}
+
+
+const subtrair = (a,b) => a - b 
+
+//callback - função de retorno
+
+
+function mostrarCallback (callback) {
+
+}
